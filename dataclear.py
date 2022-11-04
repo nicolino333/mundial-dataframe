7dcdef0c2f65aaa4b9ff1386880f948f011a25f0
nicolas <nicolasarriola89@gmail.com>
2022-11-04 12:39:54 -0300

    code more optimized in processing

--- dataclear.py
+++ dataclear.py
@@ -12,31 +12,45 @@ db=pd.read_csv('df-mundial.csv',index_col=0)
 #clear of colum name
 col_name=db['name']
 name=[]
-for j in range(0,len(col_name)):
-    new_col_name=col_name[j].replace('#','').replace('0','').replace('1','').replace('2','').replace('3','').replace('4','').replace('5','').replace('6','').replace('7','').replace('8','').replace('9','')
-    name.append(new_col_name)
+no_deseado_name=['#','0','1','2','3','4','5','6','7','8','9']
+for i in range(0,len(col_name)):
+    jugador=str(col_name[i])
+    for j in no_deseado_name:
+        if j in jugador:
+            jugador=jugador.replace(j,'')
+    name.append(jugador)
+
 
 #clear of colum high
 col_high=db['high']
+print(col_high)
 high=[]
-for j in range(0,len(col_high)):
-    new_col_high=col_high[j].replace('A','').replace('l','').replace('t','').replace('u','').replace('r','').replace('a','').replace(':','')
-    high.append(new_col_high)
+no_deseado_high=['A','l','t','u','r','a',':']
+for i in range(0,len(col_high)):
+    high_=str(col_high[i])
+    for j in no_deseado_high:
+        if j in high_:
+            high_=high_.replace(j,'')
+    high.append(high_)
+print(high[1])
+    
+
+
 
 #clear of colum weight
-col_weight=db['weight']
-weight=[]
-for j in range(0,len(col_weight)):
-    new_col_weight=col_weight[j].replace('P','').replace('e','').replace('s','').replace('o','').replace(':','')
-    weight.append(new_col_weight)
+#col_weight=db['weight']
+#weight=[]
+#for j in range(0,len(col_weight)):
+#    new_col_weight=col_weight[j].replace('P','').replace('e','').replace('s','').replace('o','').replace(':','')
+#    weight.append(new_col_weight)
 
-#we create a new dataframe cleared
-del db['high']
-del db['name']
-del db['weight']
-db['High']=high
-db['Name']=name
-db['Weight']=weight
-new_db=db.reindex(columns=['Name','Weight','High','seleccion','age'])
-print(new_db.head())
-#new_db.to_csv('MundialDataframe.csv')
+##we create a new dataframe cleared
+#del db['high']
+#del db['name']
+#del db['weight']
+#db['High']=high
+#db['Name']=name
+#db['Weight']=weight
+#new_db=db.reindex(columns=['Name','Weight','High','seleccion','age'])
+#print(new_db.head())
+##new_db.to_csv('MundialDataframe.csv')#
